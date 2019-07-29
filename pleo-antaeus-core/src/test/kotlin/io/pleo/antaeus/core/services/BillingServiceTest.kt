f1626a167cacd43261ee5f6c30faf7213b95b884
Brayoni Ian <brayoni.ian@gmail.com>
2019-07-29 22:34:32 +0300

    add tests: exceptions

--- pleo-antaeus-core/src/test/kotlin/io/pleo/antaeus/core/services/BillingServiceTest.kt
+++ pleo-antaeus-core/src/test/kotlin/io/pleo/antaeus/core/services/BillingServiceTest.kt
@@ -3,6 +3,10 @@ package io.pleo.antaeus.core.services
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.spyk
+import io.pleo.antaeus.core.exceptions.CurrencyMismatchException
+import io.pleo.antaeus.core.exceptions.CustomerNotFoundException
+import io.pleo.antaeus.core.exceptions.InvoiceNotFoundException
+import io.pleo.antaeus.core.exceptions.NetworkException
 import io.pleo.antaeus.core.external.PaymentProvider
 import io.pleo.antaeus.core.jobs.Billing
 import io.pleo.antaeus.data.AntaeusDal
@@ -10,8 +14,7 @@ import io.pleo.antaeus.models.Currency
 import io.pleo.antaeus.models.Invoice
 import io.pleo.antaeus.models.InvoiceStatus
 import io.pleo.antaeus.models.Money
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import org.quartz.*
 import java.math.BigDecimal
@@ -49,26 +52,115 @@ class BillingServiceTest {
         every { fetchInvoices() } returns paidInvoices
         every { updateInvoiceStatus(1, InvoiceStatus.PAID) } returns paidInvoices[0]
         every { updateInvoiceStatus(2, InvoiceStatus.PAID) } returns paidInvoices[1]
+        every { fetchInvoice(404) } returns null
+        every { fetchCustomer(404) } returns null
     }
 
     private val invoiceService = InvoiceService(dal = dal)
 
-    private val paymentProvider = mockk<PaymentProvider> {
-        every { charge(any()) } returns true
+    @Test
+    fun `accounts are charged`() {
+        val paymentProvider = mockk<PaymentProvider> {
+            every { charge(any()) } returns true
+        }
+
+        val billingService = BillingService(
+                job = job,
+                trigger = getBillingCronTrigger(),
+                invoiceService = invoiceService,
+                paymentProvider = paymentProvider
+        )
+
+        assertTrue(billingService.pay())
     }
 
-    private val billingService = BillingService(
-            job = job,
-            trigger = getBillingCronTrigger(),
-            invoiceService = invoiceService,
-            paymentProvider = paymentProvider
-    )
+    @Test
+    fun `account not charged on InvoiceNotFoundException occuring`() {
+        val paymentProvider = mockk<PaymentProvider> {
+            every { charge(any()) } throws InvoiceNotFoundException(404)
+        }
+
+        val billingService = BillingService(
+                job = job,
+                trigger = getBillingCronTrigger(),
+                invoiceService = invoiceService,
+                paymentProvider = paymentProvider
+        )
+
+        assertFalse(billingService.pay())
+    }
 
 
     @Test
-    fun `accounts are charged`() {
-        assertTrue(billingService.pay())
+    fun `account not charged on CustomerNotFoundException occuring`() {
+
+        val paymentProvider = mockk<PaymentProvider> {
+            every { charge(any()) } throws CustomerNotFoundException(404)
+        }
+
+        val billingService = BillingService(
+                job = job,
+                trigger = getBillingCronTrigger(),
+                invoiceService = invoiceService,
+                paymentProvider = paymentProvider
+        )
+
+        assertFalse(billingService.pay())
     }
+
+
+    @Test
+    fun `account not charged on CurrencyMismatchException occuring`() {
+
+        val paymentProvider = mockk<PaymentProvider> {
+            every { charge(any()) } throws CurrencyMismatchException(404, 404)
+        }
+
+        val billingService = BillingService(
+                job = job,
+                trigger = getBillingCronTrigger(),
+                invoiceService = invoiceService,
+                paymentProvider = paymentProvider
+        )
+
+        assertFalse(billingService.pay())
+    }
+
+    @Test
+    fun `account not charged on NetworkException`() {
+
+        val paymentProvider = mockk<PaymentProvider> {
+            every { charge(any()) } throws NetworkException()
+        }
+
+        val billingService = BillingService(
+                job = job,
+                trigger = getBillingCronTrigger(),
+                invoiceService = invoiceService,
+                paymentProvider = paymentProvider
+        )
+
+        assertFalse(billingService.pay())
+
+    }
+
+    @Test
+    fun `account not charged on any Exception occuring`() {
+
+        val paymentProvider = mockk<PaymentProvider> {
+            every { charge(any()) } throws Exception()
+        }
+
+        val billingService = BillingService(
+                job = job,
+                trigger = getBillingCronTrigger(),
+                invoiceService = invoiceService,
+                paymentProvider = paymentProvider
+        )
+
+        assertFalse(billingService.pay())
+    }
+
 
     @Test
     fun `invoices updated to paid`() {
